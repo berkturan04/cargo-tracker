33446b2fb683a46ef4e57e173d9fececaa91a9f0
Berk Turan <berkturan123@hotmail.com>
2026-09-22 12:14:08 +0300

    feat: InMemoryShipmentRepository eklendi ve DI kayıtlarını yapıldı

--- src/CargoTracker.Api/Program.cs
+++ src/CargoTracker.Api/Program.cs
@@ -1,8 +1,14 @@
+using CargoTracker.Application.Abstractions;
+using CargoTracker.Application.Services;
+using CargoTracker.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IShipmentRepository, InMemoryShipmentRepository>();
+builder.Services.AddScoped<IShipmentService, ShipmentService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
--- src/CargoTracker.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CargoTracker.Infrastructure;
-
-public class Class1
-{
-
-}
